915ffb6c361241006baecd75999460b0f5236bbf
SDunston <139296299+SDunston@users.noreply.github.com>
2024-03-22 19:00:32 -0400

    Adding Sum of the Lowest Challenge

---
+++ Sum of the lowest positive integers.js
@@ -0,0 +1,9 @@
+/* Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455. */
+
+//This problem requires me to find a way to order the numbers properly and then add two numbers together 
+
+//The first thing I need to do is put the numbers in order of highest to lowest 
